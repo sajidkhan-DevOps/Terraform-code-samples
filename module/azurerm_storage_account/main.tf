d7ab7081d5c91a4eabf5fb7a59868f8f133b15f9
Sajid Khan <sajidabdullahkhan299720@gmail.com>
2026-06-05 22:20:08 +0530

    Delete module/azurerm_storage_account directory

--- module/azurerm_storage_account/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_storage_account" "stg1" {
-  for_each = {
-    stg-one =  {
-        name = "stgaccountnew"
-        azurerm_resource_group = ""
-    }
-  }
-}
--- module/azurerm_storage_account/rg.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "charger" {
-  for_each = {
-    rg-1 = "centralindia"
-    rg-2 = "eastus"
-  }
-  name = each.key
-  location = each.value
-}
